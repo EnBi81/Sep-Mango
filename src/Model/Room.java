a0515be653482203bc25dfedc3d658729393af6b
Beatricia <315186@via.dk>
2021-11-30 19:11:02 +0100

    Creating Room class with STATIC method and so on

--- src/Model/Room.java
+++ src/Model/Room.java
@@ -1,5 +1,54 @@
 package Model;
 
-public class Room
-{
+public class Room {
+    public static ArrayList<Room> getAllAvailableRooms(LocalDateTime startTime, LocalDateTime endTime) {
+        return null;
+    }
+
+
+    private String roomNumber;
+    private int capacity;
+    private Room connectedRoom;
+
+    public Room(String roomNumber, int capacity) {
+        this.roomNumber = roomNumber;
+        this.capacity = capacity;
+        connectedRoom = null;
+    }
+
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public int getCapacity() {
+        return capacity;
+    }
+
+    public Room getConnectedRoom() {
+        return connectedRoom;
+    }
+
+    public void setConnectedRoom(Room room)
+    {
+        connectedRoom=room;
+    }
+
+    public boolean hasConnectedRoom()
+    {
+        return connectedRoom!=null;
+    }
+
+
+    public String toString() {
+
+        if(hasConnectedRoom())
+        {
+            return   roomNumber + "-" + capacity + "-" + connectedRoom.roomNumber
+
+        }
+        else return   roomNumber + "-" + capacity + "-" ;
+
+
+    }
+
 }
