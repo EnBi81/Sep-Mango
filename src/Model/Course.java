6f2bd4d83a43e37522761ee967651a34b93e2758
simonfrayer <89986425+simonfrayer@users.noreply.github.com>
2021-12-01 15:45:06 +0100

    finished Teacher, equals and toString for Course. Course should be automatically assigned to the Teacher when creating a course but not sure if its legal like this lol.

--- src/Model/Course.java
+++ src/Model/Course.java
@@ -1,5 +1,7 @@
 package Model;
 
+import utils.CollisionException;
+
 import java.util.ArrayList;
 
 public class Course {
@@ -11,11 +13,15 @@ public class Course {
   private ArrayList<Student> students;
   private Class viaClass;
 
-  public Course(String courseName, int ects, ArrayList<Teacher> teachers){
+  public Course(String courseName, int ects, ArrayList<Teacher> teachers, Class viaClass){
     this.courseName = courseName;
     this.ects = ects;
     this.teachers = teachers;
     lessons = new ArrayList<>();
+    this.viaClass = viaClass;
+    students = viaClass.getAllStudents();
+    //idk is this legal?? lol
+    assignCourseToTeacher();
   }
 
   public String getCourseName()
@@ -28,10 +34,73 @@ public class Course {
     return ects;
   }
 
-  public void addStudent(){
-    //add attributes according to the Student constructor
-    students.add(new Student());
+  public Class getViaClass(){
+    return viaClass;
   }
+
+  public void addStudent(Student student) throws CollisionException
+  {
+    //add attributes according to the Student constructor
+    students.add(student);
+  }
+
+  public void removeStudent(Student student){
+    if (students.contains(student)){
+      students.remove(student);
+
+    }
+    else {
+      System.out.println("Course " + getCourseName() + " does not contain this student.");
+    }
+  }
+
+  public void addLesson(Lesson lesson) throws CollisionException{
+    lessons.add(lesson);
+  }
+
+  public void removeLesson(Lesson lesson){
+    if (lessons.contains(lesson)){
+      lessons.remove(lesson);
+
+    }
+    else {
+      System.out.println("Course " + getCourseName() + " does not contain this lesson.");
+    }
+  }
+
+  public ArrayList<Teacher> getAllTeachers(){
+    if (teachers == null){
+      System.out.println("There are no teachers for the course " + getCourseName() + ".");
+    }
+    return teachers;
+  }
+
+  public ArrayList<Student> getAllStudents(){
+    if (students == null){
+      System.out.println("There are no students for the course " + getCourseName() + ".");
+    }
+    return students;
+  }
+
+  public ArrayList<Lesson> getAllLessons(){
+    if (lessons == null){
+      System.out.println("There are no lessons for the course " + getCourseName() + ".");
+    }
+    return lessons;
+  }
+
+  public void assignCourseToTeacher(){
+    for (Teacher teacher:teachers
+         )
+    {
+      teacher.addCourse(copy());
+    }
+  }
+
+  public Course copy(){
+    return new Course(this.getCourseName(), this.getEcts(),this.getAllTeachers(),this.getViaClass());
+  }
+
 
 
 }
--- src/Model/Teacher.java
+++ src/Model/Teacher.java
@@ -1,5 +1,7 @@
 package Model;
 
+import utils.CollisionException;
+
 import java.util.ArrayList;
 
 public class Teacher
@@ -12,6 +14,7 @@ public class Teacher
   public Teacher(String name){
     this.name = name;
     courses = new ArrayList<>();
+    lessons = new ArrayList<>();
   }
 
   public String getName()
@@ -19,13 +22,63 @@ public class Teacher
     return name;
   }
 
-  public ArrayList<Course> getCourses()
+  public void addCourse(Course course) throws CollisionException
   {
+    courses.add(course);
+  }
+
+  // do we really need this? If computer assigns it auto from the spreadsheet. Maybe if teacher would die during the semester, and they needed replacement...
+  public void removeCourse(Course course){
+    if (courses.contains(course)){
+      courses.remove(course);
+
+    }
+    else {
+      System.out.println("Teacher " + getName() + " does not teach course " + course.getCourseName() + ".");
+    }
+  }
+
+  public ArrayList<Course> getAllCourses()
+  {
+    if (courses == null){
+      System.out.println("Teacher " + getName() + " does not teach any courses.");
+    }
     return courses;
   }
 
   public ArrayList<Lesson> getAllLessons(){
+    for (Course course: courses
+         )
+    {
+      if (!(course.getAllLessons() == null)){
+        lessons.addAll(course.getAllLessons());
+      }
+    }
+    if (lessons == null){
+      System.out.println("Teacher " + getName() + " does not teach any lessons.");
+    }
+    return lessons;
+  }
 
+  public String toString(){
+    String str = "";
+
+    str += "Teacher " + getName() + "\nteaches these courses:\n";
+
+    for (Course course: courses
+         )
+    {
+      str += course.getCourseName() + "\n";
+    }
+
+    str += "and has these lessons:\n";
+
+    for (Lesson lesson: lessons
+         )
+    {
+      str += lesson.toString() + "\n";
+    }
+    return str;
   }
 
   public boolean equals(Object obj){
@@ -36,14 +89,18 @@ public class Teacher
     Teacher temp = (Teacher) obj;
 
     if (this.courses == null){
-      return this.name.equals(temp.name) && temp.courses == null;
-
+      return this.name.equals(temp.name) && temp.courses == null && this.lessons.equals(temp.lessons);
     }
 
-    // null pointer for lessons + lessons
+    if (this.lessons == null){
+      return this.name.equals(temp.name) && temp.courses == null && temp.lessons == null;
+    }
+
     else {
-      return this.name.equals(temp.name) && this.courses.equals(temp.courses);
+      return this.name.equals(temp.name) && this.courses.equals(temp.courses) && this.lessons.equals(temp.lessons);
 
     }
   }
 }
+
+
