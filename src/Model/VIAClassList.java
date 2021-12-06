b789f14350c3d526951e21378fbc9497747ddff7
Enbi81 <wotng2002@gmail.com>
2021-12-06 12:37:18 +0100

    Added Test Class Added additional Javadoc to VIAClass and VIAClassList

--- src/Model/VIAClassList.java
+++ src/Model/VIAClassList.java
@@ -3,15 +3,26 @@ package Model;
 import java.io.Serializable;
 import java.util.ArrayList;
 
+/**
+ * An object to hold multiple VIAClass objects.
+ */
 public class VIAClassList implements Serializable
 {
   private ArrayList<VIAClass> classes;
 
+  /**
+   * Creates an instance of VIAClassList
+   */
   public VIAClassList()
   {
     classes = new ArrayList<>();
   }
 
+  /**
+   * Gets the VIAClass object which has the same name as the argument
+   * @param name The VIAClass to search for
+   * @return The VIAClass object with the same name if it exists; else null.
+   */
   public VIAClass getClassByName(String name)
   {
     for (VIAClass viaClass : classes)
@@ -23,16 +34,28 @@ public class VIAClassList implements Serializable
     return null;
   }
 
+  /**
+   * Adds one VIAClass instance to the list
+   * @param viaClass The instance which should be added
+   */
   public void addVIAClass(VIAClass viaClass)
   {
     classes.add(viaClass);
   }
 
+  /**
+   * Removes a VIAClass instance from the list
+   * @param viaClass the instance which should be removed
+   */
   public void removeClass(VIAClass viaClass)
   {
     classes.remove(viaClass);
   }
 
+  /**
+   * Gets an ArrayList object with all the VIAClasses object inside
+   * @return An ArrayList object with the stored VIAClass instances
+   */
   public ArrayList<VIAClass> getAllClasses()
   {
     return classes;
---
+++ src/utils/TestClass.java
@@ -0,0 +1,167 @@
+package utils;
+
+import Model.*;
+
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
+public class TestClass
+{
+  public static void main(String[] args)
+  {
+    String folder = "Files\\",
+        coursesFile = folder + "courses.txt",
+        roomFile = folder + "rooms.txt",
+        studentFile = folder + "students.txt";
+
+    try
+    {
+      var outStream = System.out;
+      System.setOut(new PrintStream("Files\\test.txt"){
+        @Override public void println(String text)
+        {
+          // Prints nothing
+        }
+      });
+
+      Schedule schedule = ImportData.importFromFiles(roomFile, coursesFile, studentFile);
+
+      System.setOut(outStream);
+
+      test(schedule);
+
+    }catch (FileNotFoundException e)
+    {
+      e.printStackTrace();
+    }
+  }
+
+  public static void test(Schedule schedule)
+  {
+    try
+    {
+      writeSeparatorSpace();
+      writeRoomList(schedule.getRoomList());
+      writeSeparatorSpace();
+      writeStudentList(schedule.getStudentList());
+      writeSeparatorSpace();
+      writeTeacherList(schedule.getTeacherList());
+      writeSeparatorSpace();
+      writeLessonList(schedule.getLessonList());
+      writeSeparatorSpace();
+      writeCourseList(schedule.getCourseList());
+      writeSeparatorSpace();
+      writeClassList(schedule.getVIAClassList(), false);
+
+    }catch (Exception e)
+    {
+      System.out.println(e.getMessage());
+      e.printStackTrace();
+    }
+  }
+
+  private static void writeRoomList(RoomList roomList)
+  {
+    write("RoomList" + intToParentheses(roomList.getAllRooms().size()), 0);
+    for (var room : roomList.getAllRooms())
+      writeRoom(room);
+  }
+  private static void writeRoom(Room room)
+  {
+    write(room.getRoomName(), 1);
+    write("Capacity: " + room.getCapacity(), 2);
+    write("Has connected room: " + room.hasConnectedRoom(), 2);
+  }
+
+  private static void writeStudentList(StudentList studentList)
+  {
+    write("StudentList" + intToParentheses(studentList.getAllStudents().size()), 0);
+    for (var student : studentList.getAllStudents())
+    {
+      writeStudent(student, 0);
+    }
+  }
+  private static void writeStudent(Student student, int level)
+  {
+    write(student.getName() + intToParentheses(student.getId()), level + 1);
+  }
+
+  private static void writeTeacherList(TeacherList teacherList)
+  {
+    write("TeacherList" + intToParentheses(teacherList.getAllTeachers().size()),0);
+    for (var teacher : teacherList.getAllTeachers())
+      writeTeacher(teacher, 0);
+
+  }
+  private static void writeTeacher(Teacher teacher, int level)
+  {
+    write(teacher.getName(), level + 1);
+  }
+
+  private static void writeLessonList(LessonList lessonList)
+  {
+    write("LessonList" + intToParentheses(lessonList.getAllLessons().size()), 0);
+    write("Lesson count (should be 0): " + lessonList.getAllLessons().size(), 1);
+  }
+
+  private static void writeCourseList(CourseList courseList)
+  {
+    write("CourseList" + intToParentheses(courseList.getAllCourses().size()), 0);
+    for (var course : courseList.getAllCourses())
+    {
+      writeCourse(course, 0);
+    }
+  }
+  private static void writeCourse(Course course, int level)
+  {
+    write(course.getCourseName(), level + 1);
+    write("Ects: " + course.getEcts(), level + 2);
+    write("Class: " + (course.getVIAClass() == null ? "null" :
+        course.getVIAClass().getName()), level + 2);
+    write("Teachers" + intToParentheses(course.getAllTeachers().size()), level + 2);
+    for (var teacher : course.getAllTeachers())
+      writeTeacher(teacher, level + 2);
+    write("Students" + intToParentheses(course.getAllStudents().size()), level + 2);
+    for (var student : course.getAllStudents())
+      writeStudent(student, level + 2);
+  }
+
+  private static void writeClassList(VIAClassList viaClassList, boolean writeFullCourse)
+  {
+    write("VIAClassList" + intToParentheses(viaClassList.getAllClasses().size()), 0);
+    for (var viaClass : viaClassList.getAllClasses())
+      writeClass(viaClass, writeFullCourse);
+  }
+  private static void writeClass(VIAClass viaClass, boolean writeFullCourse)
+  {
+    write("Class " + viaClass.getName(), 1);
+    write("Students" + intToParentheses(viaClass.getAllStudents().size()), 2);
+    for (var student : viaClass.getAllStudents())
+      writeStudent(student, 2);
+    write("Courses" + intToParentheses(viaClass.getAllCourses().size()), 2);
+    for (var course : viaClass.getAllCourses())
+    {
+      if(writeFullCourse)
+        writeCourse(course, 2);
+      else write(course.getCourseName(), 3);
+    }
+  }
+
+
+
+
+  private static String intToParentheses(int num)
+  {
+    return " (" + num + ")";
+  }
+  private static void writeSeparatorSpace()
+  {
+    System.out.println();
+    System.out.println();
+  }
+  private static void write(String text, int level)
+  {
+    System.out.println("\t".repeat(level) + text);
+  }
+
+}
